ca076c31a2d0a30d6f6af8db6c52adae4a178d0d
bkenobi <bkenobi69@yahoo.com>
2020-11-25 13:02:28 -0800

    Update Board.cs

--- Raspberry.System/Board.cs
+++ Raspberry.System/Board.cs
@@ -44,7 +44,7 @@ namespace Raspberry
         /// </summary>
         public static Board Current
         {
-            Console.WriteLine("Inside  Boards.cs Board Current");
+            ///Console.WriteLine("Inside  Boards.cs Board Current");
             get { return board.Value; }
         }
 
@@ -56,7 +56,7 @@ namespace Raspberry
         /// </value>
         public bool IsRaspberryPi
         {
-            Console.WriteLine("Inside  Boards.cs IsRaspberryPi");
+            ///Console.WriteLine("Inside  Boards.cs IsRaspberryPi");
             get
             {
                 return Processor != Processor.Unknown;
@@ -71,7 +71,7 @@ namespace Raspberry
         /// </value>
         public string ProcessorName
         {
-            Console.WriteLine("Inside  Boards.cs ProcessorName");
+            ///Console.WriteLine("Inside  Boards.cs ProcessorName");
             get
             {
                 string hardware;
@@ -87,7 +87,7 @@ namespace Raspberry
         /// </value>
         public Processor Processor
         {
-            Console.WriteLine("Inside  Boards.cs Processor");
+            ///Console.WriteLine("Inside  Boards.cs Processor");
             get
             {
                 Processor processor;
@@ -113,7 +113,7 @@ namespace Raspberry
         /// </summary>
         public int Firmware
         {
-            Console.WriteLine("Inside  Boards.cs Firmware");
+            ///Console.WriteLine("Inside  Boards.cs Firmware");
             get
             {
                 string revision;
@@ -132,7 +132,7 @@ namespace Raspberry
         /// </summary>
         public string SerialNumber
         {
-            Console.WriteLine("Inside  Boards.cs SerialNumber");
+            ///Console.WriteLine("Inside  Boards.cs SerialNumber");
             get {
                 string serial;
                 if (settings.TryGetValue("Serial", out serial)
@@ -151,7 +151,7 @@ namespace Raspberry
         /// </value>
         public bool IsOverclocked
         {
-            Console.WriteLine("Inside  Boards.cs IsOverclocked");
+            ///Console.WriteLine("Inside  Boards.cs IsOverclocked");
             get
             {
                 var firmware = Firmware;
@@ -167,7 +167,7 @@ namespace Raspberry
         /// </value>
         public Model Model
         {
-            Console.WriteLine("Inside  Boards.cs Model");
+            ///Console.WriteLine("Inside  Boards.cs Model");
             get { return model.Value; }
         }
 
@@ -180,7 +180,7 @@ namespace Raspberry
         /// <remarks>See <see cref="http://raspi.tv/2014/rpi-gpio-quick-reference-updated-for-raspberry-pi-b"/> for more information.</remarks>
         public ConnectorPinout ConnectorPinout
         {
-            Console.WriteLine("Inside  Boards.cs ConnectorPinout");
+            ///Console.WriteLine("Inside  Boards.cs ConnectorPinout");
             get { return connectorPinout.Value; }
         }
 
@@ -190,7 +190,7 @@ namespace Raspberry
 
         private static Board LoadBoard()
         {
-            Console.WriteLine("Inside  Boards.cs Board LoadBoard");
+            ///Console.WriteLine("Inside  Boards.cs Board LoadBoard");
             try
             {
                 const string filePath = "/proc/cpuinfo";
@@ -227,7 +227,7 @@ namespace Raspberry
 
         private Model LoadModel()
         {
-            Console.WriteLine("Inside  Boards.cs Model LoadModel");
+            ///Console.WriteLine("Inside  Boards.cs Model LoadModel");
             var firmware = Firmware;
             Console.WriteLine("Firmware = {0:x}", firmware);
             Console.WriteLine("Firmware & 0xFFFF = {0:x}", firmware & 0xFFFF);
@@ -297,7 +297,7 @@ namespace Raspberry
 
         private ConnectorPinout LoadConnectorPinout()
         {
-            Console.WriteLine("Inside  Boards.cs ConnectorPinout LoadConnectorPinout");
+            ///Console.WriteLine("Inside  Boards.cs ConnectorPinout LoadConnectorPinout");
             switch (Model)
             {
                 case Model.BRev1:
